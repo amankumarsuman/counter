c8c322159e5c632522330a3e518ff81efbddaf74
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2022-09-22 18:55:21 +0530

    done

---
+++ src/components/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import "./counterStyle.css"
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const [pause, setPause] = useState(false);
+
+  function handlePause() {
+    setPause(!pause);
+  }
+
+  
+
+  useEffect(() => {
+    let interval = null;
+    if (pause) {
+      interval = setInterval(() => {
+        setCounter(counter => counter + 1);
+      }, 1000);
+    } else if (!pause && counter !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [pause, counter]);
+
+  return (
+    <div className="mainDiv">
+      <h1 className="counter">
+        {counter}
+      </h1>
+      <div >
+        <button className={`button button-${pause ? 'start' : 'pause'}`} onClick={handlePause}>
+          {pause ? 'pause' : 'Start'}
+        </button>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
